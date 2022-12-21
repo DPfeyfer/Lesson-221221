47143e544d2e218e666802ca50a17f1a49d02305
DPfeyfer <Skydenfer737@gmail.com>
2022-12-21 13:36:04 +0300

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int number = 595;
         System.out.println(number);
+        byte number1 = -128;
+        System.out.println(number1);
     }}

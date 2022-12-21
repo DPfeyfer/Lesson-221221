78e8ec38957031dca8922559e78e1a767a582e58
DPfeyfer <Skydenfer737@gmail.com>
2022-12-21 13:54:50 +0300

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 595;
-        System.out.println(number);
-        byte number1 = -128;
-        System.out.println(number1);
+        int dollars = 3465000;
+        System.out.println("The value of a variable dollars with type int is " + dollars);
+        byte temperatureMoon = -128;
+        System.out.println("The value of a temperatureMoon with type byte is " + temperatureMoon);
+        short number2 = 13525;
+        System.out.println(number2);
     }}

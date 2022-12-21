bd79dbcef25f79a97687982640b5a2c422a6aeea
DPfeyfer <Skydenfer737@gmail.com>
2022-12-21 14:45:29 +0300

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
         System.out.println("The value of a variable dollars with type int is " + dollars);
         byte temperatureMoon = -128;
         System.out.println("The value of a temperatureMoon with type byte is " + temperatureMoon);
-        short number2 = 13525;
-        System.out.println(number2);
+        short marianaTrench = 10994;
+        System.out.println("The value of a marianaTrench with type short is " + marianaTrench);
+        long sunDistance = 1495978707;
+        System.out.println("The value of a sunDistance with type long is " + sunDistance);
+        float pixelSize = 0.297f;
+        System.out.println("The value of a pixelSize with type float is " + pixelSize);
+        double waterMolecule = 0.00000000003;
+        System.out.println("The value of a waterMolecule with type double is " + waterMolecule);
     }}
